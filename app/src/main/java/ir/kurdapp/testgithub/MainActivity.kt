dd7d72a143173f8e1dbe99537b951441a050417c
Sina <sinagamer502@gmail.com>
2023-06-11 01:00:33 +0330

    one commit2

--- app/src/main/java/ir/kurdapp/testgithub/MainActivity.kt
+++ app/src/main/java/ir/kurdapp/testgithub/MainActivity.kt
@@ -2,10 +2,28 @@ package ir.kurdapp.testgithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import ir.kurdapp.testgithub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate( layoutInflater )
+        setContentView( binding.root )
+
+
+        binding.button.setOnClickListener {
+
+            val dialog = AlertDialog.Builder( this ).create()
+            dialog.setTitle( "hello my name is sina" )
+            dialog.show()
+
+        }
+
+
+
+
+
     }
 }
